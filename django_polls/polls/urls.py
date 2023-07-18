5d490360dc98b214174d4cf4b0b7853d5a8c7b28
Javier Casapia <javiercasapia97@gmail.com>
2023-07-18 15:42:00 -0500

    crear rutas y vistas

--- django_polls/polls/urls.py
+++ django_polls/polls/urls.py
@@ -2,5 +2,28 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    # ex: /polls/
+    path(
+        route="",
+        view=views.index,
+        name="index"
+    ),
+    # ex: /polls/5/
+    path(
+        route="<int:question_id>/",
+        view=views.detail,
+        name="detail"
+    ),
+    # ex: /polls/5/results/
+    path(
+        route="<int:question_id>/results/",
+        view=views.results,
+        name="results"
+    ),
+    # ex: /polls/5/vote/
+    path(
+        route="<int:question_id>/vote/",
+        view=views.vote,
+        name="vote"
+    ),
 ]
