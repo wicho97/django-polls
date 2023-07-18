79686d716bd75c05b6840038d5086451de69caaf
Javier Casapia <javiercasapia97@gmail.com>
2023-07-18 16:51:27 -0500

    agregar template 'deatil.html', modificar view 'detail'

--- django_polls/polls/views.py
+++ django_polls/polls/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from django.template import loader
 from .models import Question
 
 # Create your views here.
@@ -15,7 +14,8 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
