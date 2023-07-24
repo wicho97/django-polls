63844fe938ebe3253518ba72e28b429113b8434b
Javier Casapia <javiercasapia97@gmail.com>
2023-07-24 16:39:30 -0500

    agregar paginacion

--- django_polls/polls/views.py
+++ django_polls/polls/views.py
@@ -18,13 +18,14 @@ DEFAULT_LIMIT = 10
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
+    paginate_by = 5
 
     def get_queryset(self):
         """
         Return the last five published questions (not including those set to be
         published in the future).
         """
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:DEFAULT_LIMIT]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:10]
 
 
 class DetailView(generic.DetailView):
