990ae708b64c5945c44592ce4c23a1914e98c9b8
Javier Casapia <javiercasapia97@gmail.com>
2023-07-31 11:29:27 -0500

    vistas para cambiar contrasena

--- django_polls/account/urls.py
+++ django_polls/account/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 app_name = 'account'
 urlpatterns = [
+    # login / logout urls
     path(
         route='login/',
         view=auth_views.LoginView.as_view(),
@@ -13,6 +14,17 @@ urlpatterns = [
         route='logout/',
         view=auth_views.LogoutView.as_view(),
         name='logout'
+    ),
+    # change password urls
+    path(
+        route='password-change/',
+        view=auth_views.PasswordChangeView.as_view(),
+        name='password_change'
+    ),
+    path(
+        route='password-change/done/',
+        view=auth_views.PasswordChangeDoneView.as_view(),
+        name='password_change_done'
     ),
     path(
         route='',
